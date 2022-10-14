388ffb1a4de429955d952b8fac8053a44d27810c
andre1400 <andre.brmo@hotmail.com>
2022-10-14 09:52:29 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 echo "Hello World"
 sudo hostnamectl set-hostname SRV-LIN1-02
+cat <<EOF > /etc/network/interface
+# This file describes the network interfaces available on your system
+# and how to activate them. For more information, see interfaces(5).
+
+source /etc/network/interfaces.d/*
+
+# The loopback network interface
+auto lo
+iface lo inet loopback
+
+# The primary network interface
+auto hotplug ens33
+iface ens33 inet static
+address 10.10.10.22
+netmask 255.255.255.0
+gateway 10.10.10.2
+EOF
