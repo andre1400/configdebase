4d2aad44c35fc5f8fd7ce87de4c70af5d11c9dad
andre1400 <andre.brmo@hotmail.com>
2022-10-14 10:13:46 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo "Hello World"
 sudo hostnamectl set-hostname SRV-LIN1-02
-sudo cat <<EOF > /etc/network/interface
+# bash -c (interpréter la commande -c , pour executer la commande cat)(<< toutes ses lignes qui vont etre écrites dans /etc/network/interfaces qui sera ecraser grace à ">")
+sudo bash -c "cat << EOF > /etc/network/interfaces
 # This file describes the network interfaces available on your system
 # and how to activate them. For more information, see interfaces(5).
 
@@ -17,4 +18,4 @@ iface ens33 inet static
 address 10.10.10.22
 netmask 255.255.255.0
 gateway 10.10.10.2
-EOF
+EOF"
