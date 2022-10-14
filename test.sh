be59af0148edbf2770d581e9c2829b69bf7412be
andre1400 <andre.brmo@hotmail.com>
2022-10-14 10:48:09 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-echo "Hello World"
+echo "Welcome to my World"
+
+# Changer le hostname
 sudo hostnamectl set-hostname SRV-LIN1-02
-# bash -c (interpréter la commande -c , pour executer la commande cat)(<< toutes ses lignes qui vont etre écrites dans /etc/network/interfaces qui sera ecraser grace à ">")
+
+# Interface ip static
+#bash -c (interpréter la commande -c , pour executer la commande cat)(<< toutes ses lignes qui vont etre écrites dans /etc/network/interfaces qui sera ecraser grace à ">")
+interfaces_F="/etc/network/interfaces"
 sudo bash -c "cat << EOF > /etc/network/interfaces
 # This file describes the network interfaces available on your system
 # and how to activate them. For more information, see interfaces(5).
@@ -19,3 +24,15 @@ address 10.10.10.22
 netmask 255.255.255.0
 gateway 10.10.10.2
 EOF"
+
+#Set resolv.cong pour le dns
+resolv_F="/etc/resolv.conf"
+
+sudo bash -c "cat <<EOM >$resolv_F
+
+domain lin1.local
+search lin1.local
+nameserver 10.10.10.11
+nameserver 10.10.10.2
+
+EOM"
